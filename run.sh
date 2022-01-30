a7d51e7b2c0b6f9f50ff39976685f468cde5127f
Ronald <ronaldvalenzuel@gmail.com>
2022-01-30 11:13:32 -0300

    Automation

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ADDERS=('carryRippleN_h carrySkip carryLookAhead')
+mkdir -p work
+cd work
+# Execute
+for adder in ${ADDERS[@]}; do 
+	make -f ../scripts/Makefile  $adder TOP_MODULE=$adder
+	make -f ../scripts/Makefile sta NETLIST=results/$adder.vg TOP_MODULE=$adder
+done
+cd -
+# Collect Results
+echo "Design,Arrival" > results.rpt
+for adder in ${ADDERS[@]}; do 
+	echo "$adder,$(grep arrival work/reports/$adder.sta -m1 | awk '{print $1}')" >> results.rpt	
+done
